cb5e84cbd3584735439e8c0aa49a165aeaf017a3
Imad Asserne <imad.asserne@gmail.com>
2023-11-21 21:37:48 +0100

    add new relation with webOrderQuantities

--- src/main/java/com/IMADWRGH/ecommercebackend/model/WebOrder.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/model/WebOrder.java
@@ -1,17 +1,19 @@
 package com.IMADWRGH.ecommercebackend.model;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "order")
-public class Order {
+@Table(name = "web_order")
+public class WebOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
 
-    ////////Relationship with LocalUser and Address /////////
+    ////////Relationship with LocalUser and Address and WebOrderQuantities////////
     @ManyToOne(optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private LocalUser user;
@@ -19,8 +21,11 @@ public class Order {
     @JoinColumn(name = "address_id", nullable = false)
     private Address address;
 
+    @OneToMany(mappedBy = "order" ,cascade = CascadeType.REMOVE,orphanRemoval = true)
+    private List<WebOrderQuantities> quantities =new ArrayList<>();
 
     //////////Getter & Setter/////////////
+
 
     public Long getId() {
         return id;
@@ -44,5 +49,13 @@ public class Order {
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    public List<WebOrderQuantities> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(List<WebOrderQuantities> quantities) {
+        this.quantities = quantities;
     }
 }
