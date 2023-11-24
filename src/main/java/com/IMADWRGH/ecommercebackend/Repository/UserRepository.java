aef1d62e7a1eb9c124e71a3528b610564d1a3363
Imad Asserne <imad.asserne@gmail.com>
2023-11-24 03:51:20 +0100

    add 2 function

--- src/main/java/com/IMADWRGH/ecommercebackend/Repository/UserRepository.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/Repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.IMADWRGH.ecommercebackend.model.LocalUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<LocalUser,Long> {
+    Optional<LocalUser> findByUserNameIgnoreCase(String username);
+    Optional<LocalUser> findByEmailIgnoreCase(String email);
 }
