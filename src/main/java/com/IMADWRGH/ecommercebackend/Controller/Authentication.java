7aa78a8db5e01239bc2150757f3f8f96c71ab246
Imad Asserne <imad.asserne@gmail.com>
2023-11-24 03:54:52 +0100

    inject UserService & change function "RegisterUser"

--- src/main/java/com/IMADWRGH/ecommercebackend/Controller/Authentication.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/Controller/Authentication.java
@@ -1,16 +1,29 @@
 package com.IMADWRGH.ecommercebackend.Controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.IMADWRGH.ecommercebackend.Exception.UserException;
+import com.IMADWRGH.ecommercebackend.Service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/auth")
 public class Authentication {
+    private final UserService userService;
+    @Autowired
+    public Authentication(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping(path = "/register")
-    public void RegisterUser(@RequestBody RegistrationBody registrationBody){
-
+    public ResponseEntity RegisterUser(@Valid @RequestBody RegistrationBody registrationBody)  {
+        try {
+            userService.Register(registrationBody);
+            return ResponseEntity.ok().build();
+        } catch (UserException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
