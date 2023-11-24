764b0a911c9f3c6232e3e81780e97dddca5761f5
Imad Asserne <imad.asserne@gmail.com>
2023-11-24 03:55:40 +0100

    inject UserRepository & add function "Register"

--- src/main/java/com/IMADWRGH/ecommercebackend/Service/UserService.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/Service/UserService.java
@@ -1,7 +1,30 @@
 package com.IMADWRGH.ecommercebackend.Service;
 
+import com.IMADWRGH.ecommercebackend.Controller.RegistrationBody;
+import com.IMADWRGH.ecommercebackend.Exception.UserException;
+import com.IMADWRGH.ecommercebackend.Repository.UserRepository;
+import com.IMADWRGH.ecommercebackend.model.LocalUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void Register(RegistrationBody registrationBody) throws UserException {
+        if (userRepository.findByUserNameIgnoreCase(registrationBody.getUsername()).isPresent()
+                || userRepository.findByEmailIgnoreCase(registrationBody.getEmail()).isPresent()) {
+           throw new UserException();
+        }
+        LocalUser localUser = new LocalUser();
+        localUser.setFullName(registrationBody.getFullname());
+        localUser.setEmail(registrationBody.getEmail());
+        localUser.setPassword(registrationBody.getPassword());
+        localUser.setUserName(registrationBody.getUsername());
+        userRepository.save(localUser);
+    }
 }
