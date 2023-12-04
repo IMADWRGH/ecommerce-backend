029df0689af7e9002422f26bae839303bb3ef86e
Imad Asserne <imad.asserne@gmail.com>
2023-12-04 04:56:04 +0100

    add annotation "@Repository"

--- src/main/java/com/IMADWRGH/ecommercebackend/Repository/WebOrderQntRepository.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/Repository/WebOrderQntRepository.java
@@ -2,6 +2,8 @@ package com.IMADWRGH.ecommercebackend.Repository;
 
 import com.IMADWRGH.ecommercebackend.model.WebOrderQuantities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WebOrderQntRepository extends JpaRepository<WebOrderQuantities,Long> {
 }
