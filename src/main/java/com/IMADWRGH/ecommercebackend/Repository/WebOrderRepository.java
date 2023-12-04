7b5998606dd4e8cc05a60fb91688a72518ed349f
Imad Asserne <imad.asserne@gmail.com>
2023-12-04 04:57:10 +0100

    add annotation "@Repository" & function "findByUser"

--- src/main/java/com/IMADWRGH/ecommercebackend/Repository/WebOrderRepository.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/Repository/WebOrderRepository.java
@@ -1,7 +1,12 @@
 package com.IMADWRGH.ecommercebackend.Repository;
 
+import com.IMADWRGH.ecommercebackend.model.LocalUser;
 import com.IMADWRGH.ecommercebackend.model.WebOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface WebOrderRepository extends JpaRepository<WebOrder,Long> {
+    List<WebOrder> findByUser(LocalUser user);
 }
