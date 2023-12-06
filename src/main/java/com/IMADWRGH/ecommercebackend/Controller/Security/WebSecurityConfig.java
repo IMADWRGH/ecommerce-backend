5a12feb450ac2b82714f492196efeebb105addbe
Imad Asserne <imad.asserne@gmail.com>
2023-12-06 23:32:11 +0100

    Endpoint security

--- src/main/java/com/IMADWRGH/ecommercebackend/Controller/Security/WebSecurityConfig.java
+++ src/main/java/com/IMADWRGH/ecommercebackend/Controller/Security/WebSecurityConfig.java
@@ -3,17 +3,28 @@ package com.IMADWRGH.ecommercebackend.Controller.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
 
 @Configuration
 public class WebSecurityConfig {
+    private  final  JwtRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
-                );
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtRequestFilter,  AuthorizationFilter.class);
+                http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                    .anyRequest().authenticated()
+        );
 
         return http.build();
     }
